8a0882bc6692b888ed351b24f856957466668352
hezerong <hezerongandy@163.com>
2016-06-24 20:55:33 +0800

    创建项目

---
+++ InvoiceCheck/InvoiceCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InvoiceCheck
+//
+//  Created by hezerong on 16/6/24.
+//  Copyright © 2016年 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InvoiceCheck/InvoiceCheck/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InvoiceCheck
+//
+//  Created by hezerong on 16/6/24.
+//  Copyright © 2016年 Rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
